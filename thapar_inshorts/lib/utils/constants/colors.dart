96a476205ce7c9d2aa20c118a6d64c6de8b3f25d
Pvk1294 <88473225+Pvk1294@users.noreply.github.com>
2024-06-25 14:26:11 +0530

    video 3 fully not completed now direct splash screen

--- thapar_inshorts/lib/utils/constants/colors.dart
+++ thapar_inshorts/lib/utils/constants/colors.dart
@@ -20,7 +20,20 @@ class TColors {
 
   // Text Colors
   static const Color textPrimary = Color(0xFF333333);
-  static const Color textSecondary = Color(0xFF333333);
-  static const Color textaccent = Color(0xFF333333);
-  
+  static const Color textSecondary = Color(0xFF6C7570);
+  static const Color textwhite = Colors.white;
+
+  //background color
+  static const Color light = Color(0xFFF6F6F6);
+  static const Color dark = Color(0xFF272727);
+  static const Color primaryBackground = Color(0xFFF3F5FF);
+
+  //Button Colors
+  static const Color buttonPrimary = Color(0xFF4b68ff);
+  static const Color buttonSecondary = Color(0xFF6C7570);
+  static const Color buttonDisabled = Color(0xFFC4C4C4);
+
+  //border color
+  static const Color borderPrimary = Color(0xFFD9D9D9);
+  static const Color borderSecondary = Color(0xFFE6E6E6);
 }
---
+++ thapar_inshorts/lib/utils/constants/image_strings.dart
@@ -0,0 +1,10 @@
+// This class contains all the app images in string formats
+class TImages {
+  // ----App Logos----
+  static const String darkAppLogo = "";
+  static const String lightAppLogo = "";
+
+  // ----Social Logos----
+  static const String google = "";
+  static const String facebook = "";
+}
